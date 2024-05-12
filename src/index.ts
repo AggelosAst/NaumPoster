7b62bf5d981e5447f8ed98b21e5347af829a2501
Aggelos <endmysuffering773@gmail.com>
2024-05-12 19:13:32 +0300

    Fix program oopsie!

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,14 @@
 import {Server} from "./libs/Server";
 import {Database} from "./libs/Database";
-import {DateTime} from "luxon";
 
-(async () => {
-    const db: Database = new Database();
-    await db.connect().then(_ => {
-        const server: Server = new Server({
-            port: 80
-        })
-        server.startServer()
+
+const db: Database = new Database();
+db.connect().then(_ => {
+    const server: Server = new Server({
+        port: 80
     })
-})()
+    server.startServer()
+})
+
 
 
